f1de3d1bd9e2b8eb4ef7ec54ceb64827bd20e67e
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-01-27 18:58:02 -0700

    Implimented custom Bootstrap-based theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,18 +2,58 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Chris Kankiewicz'
-SITENAME = u'Chris Kankiewicz'
-SITEURL = ''
+# Site info
+AUTHOR       = 'Chris Kankiewicz'
+SITENAME     = 'Chris Kankiewicz'
+SITEBLURB    = 'Thoughts and ramblings of a web geek'
+AUTHOR_EMAIL = 'Chris@ChrisKankiewicz.com'
+SITEURL      = 'http://blog.chriskankiewicz.com'
 
-TIMEZONE = 'Europe/Paris'
+# Site timezone
+TIMEZONE = 'America/Phoenix'
 
-DEFAULT_LANG = u'en'
+# Set defaults
+DEFAULT_LANG        = 'en'
+DEFAULT_CATEGORY    = 'Uncategorized'
+DEFAULT_DATE_FORMAT = ('%B %d, %Y - %I:%M %p')
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+# FEED_ALL_ATOM         = None
+# CATEGORY_FEED_ATOM    = None
 TRANSLATION_FEED_ATOM = None
+
+# Theming
+THEME = 'themes/bootstrapped'
+
+SUMMARY_MAX_LENGTH = 80
+
+# Plugins
+PLUGIN_PATH = 'plugins'
+PLUGINS = [
+    'googleplus_comments',
+    'gravatar',
+    'related_posts',
+    'share_post',
+    'sitemap'
+]
+
+# Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+# Static Paths
+# STATIC_PATHS = ['images']
 
 # Blogroll
 LINKS =  (
@@ -23,12 +63,16 @@ LINKS =  (
 
 # Social widget
 SOCIAL = (
-    ('Google+', '#'),
-    ('Twitter', '#'),
-    ('GitHub', '#')
+    ('Google+', 'https://plus.google.com/+ChrisKankiewicz'),
+    ('Twitter', 'https://twitter.com/PHLAK'),
+    ('GitHub', 'https://github.com/PHLAK')
 )
 
+# Pagination
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# Output retention
+OUTPUT_RETENTION = ('.gitignore')
