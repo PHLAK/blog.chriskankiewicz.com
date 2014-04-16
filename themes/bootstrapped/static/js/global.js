0db71a0f5f0dc1f5beeb3b5e41d1640917798c70
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-04-16 13:06:58 -0700

    Fixed 404ing request for global.js and updated global.js contents

--- themes/bootstrapped/static/js/global.js
+++ themes/bootstrapped/static/js/global.js
@@ -3,28 +3,4 @@ $(document).ready(function() {
     // Enable Tooltips
     $('[rel="tooltip"]').tooltip();
 
-    // Site search on submit action
-    $('.site-search').submit(function(event) {
-
-        // Prevent default action
-        event.preventDefault();
-
-        // Get the search text
-        var searchText = $('.site-search .search-text').val().trim();
-
-        if (searchText !== '') {
-
-            // URL encode the search text
-            var encodedText = encodeURIComponent(searchText).replace(/%20/g, '+');
-
-            // Create the full search URL
-            var searchURL = 'https://encrypted.google.com/#q=' + siteURL + '+' + encodedText;
-
-            // Redirect page
-            window.location.replace(searchURL);
-
-        }
-
-    })
-
 });
